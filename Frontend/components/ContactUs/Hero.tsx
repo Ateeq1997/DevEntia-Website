a7d4673f6fbf395856eed1f05f517eab3ba3fb01
tauseefmalk <tauseefarshad23@icloud.com>
2025-01-09 00:30:39 +0500

    change hero content

--- Frontend/components/ContactUs/Hero.tsx
+++ Frontend/components/ContactUs/Hero.tsx
@@ -16,15 +16,14 @@ const Hero: React.FC = () => {
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
         priority
       />
-      <p>Welcome To <span className='text-[#0077FF] font-bold text-lg'>Deventia</span></p>
+      <p>Welcome To <span className='text-[#0077FF] font-bold text-lg'>Deventia Tech</span></p>
       <h1 className="z-10 text-white text-center md:text-start text-[3rem] md:text-[5rem] font-bold leading-tight">
         Let&apos;s Talk!
       </h1>
       <hr className="w-1/3 h-1 bg-[#8787FC] rounded-full border-none" />
       
         <p className="text-white text-center z-10 max-w-2xl px-3 ">
-        Lorem ipsum dolor sit amet consectetur. Aliquam quisque cursus proin adipiscing quis diam. 
-        eu fringilla elit commodo nec aliquet. Augue pharetra nibh co
+        Where Innovation Meets Imagination
       </p>
     </header>
   );
