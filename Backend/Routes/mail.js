a65e6316d62de8c25c760d574a3417a321d61610
ABDUL MAJID <abdulmajid@ABDULs-MacBook-Air.local>
2025-05-27 20:57:56 +0500

    dsa

--- Backend/Routes/mail.js
+++ Backend/Routes/mail.js
@@ -3,6 +3,7 @@ const sendMail = require('../controller/mail/sendMail');
 
 const router = express.Router();
 
+// send acknowledgment email
 router.post('/mail', sendMail);
 
 module.exports = router;
