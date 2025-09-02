dffbf562b54923645e163c7d2f7586a738c412d2
kainat robi <kainatrobi01@gmail.com>
2025-09-02 11:29:28 +0500

    changes

--- Frontend/components/home/tech.tsx
+++ Frontend/components/home/tech.tsx
@@ -7,7 +7,7 @@ const Tech =()=>{
              <p className="text-[#4848FF] text-[22px] text-center">TECH</p>
              <h1 className="text-center font-bold text-[32px] lg:text-[63px]">Our Core Development Stack</h1>
              <p className="text-[#B8BBD2] text-[16px] text-center">We use a modern, reliable tech stack to build fast, secure, and scalable digital solutions tailored to your needs.</p>
-             <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 mt-16 mb-6 gap-6 place-items-center">
+             <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 mt-16 mb-16 gap-6 place-items-center">
   <img
     src="/home/java.png"
     alt="Java"
