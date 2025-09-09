08089e3266afa3e9c561d79ac096dd3378f637ed
kainat robi <kainatrobi01@gmail.com>
2025-09-09 12:10:56 +0500

    changes

--- Frontend/components/Casestudy/userstoryboard.tsx
+++ Frontend/components/Casestudy/userstoryboard.tsx
@@ -12,29 +12,29 @@ interface UserStoryboardProps {
 
 const UserStoryboard: React.FC<UserStoryboardProps> = ({ cards }) => {
   return (
-    <div className="px-[5%] py-6  flex flex-col items-center justify-center">
+    <div className="px-[5%] py-6 flex flex-col items-center justify-center">
       <h1 className="text-[24px] md:text-[56px] font-inter font-bold text-white">
         User Storyboard
       </h1>
       <div className="w-[200px] sm:w-[380px] h-[2px] bg-white mx-auto mt-0" />
       <div className="w-[160px] sm:w-[340px] h-[2px] bg-white mx-auto mt-2" />
 
-      {/* Outer rounded container with bg clipping */}
+      {/* Outer rounded container with bg */}
       <div
-        className="mt-12 rounded-2xl overflow-hidden w-full py-16 
+        className="mt-12 rounded-2xl overflow-hidden w-full py-16 px-6
              bg-[url('/casestudy/storybg.png')] bg-cover bg-center bg-no-repeat"
       >
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {cards.map((card, index) => (
             <div
               key={index}
-              className="flex flex-col items-center text-center rounded-xl   "
+              className="flex flex-col items-center text-center rounded-xl p-6"
             >
-              <div className="border border-[#3B3BD3] rounded-lg overflow-hidden w-full h-full lg:w-[349px] lg:h-[320px] mb-6">
+              <div className="w-full h-full lg:w-[349px] lg:h-[370px] mb-4">
                 <img
                   src={card.image}
                   alt={card.description}
-                  className="w-full h-full object-cover"
+                  className="w-full h-full object-cover rounded-lg"
                 />
               </div>
               <p className="text-gray-300 text-sm md:text-[20px] mt-2 font-inter">
