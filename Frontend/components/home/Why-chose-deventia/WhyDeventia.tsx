229063486ea7fc5ee1c03f6b8e38a237f5c30e71
kainat robi <kainatrobi01@gmail.com>
2025-09-11 12:18:44 +0500

    changes

--- Frontend/components/home/Why-chose-deventia/WhyDeventia.tsx
+++ Frontend/components/home/Why-chose-deventia/WhyDeventia.tsx
@@ -11,14 +11,17 @@ const WhyDeventia = () => {
       {/* Heading */}
       <div className="relative mx-auto text-left">
         <p className="text-[#4E6BFF] text-lg font-semibold">Our Services</p>
-        <h2 className="text-3xl md:text-[63px] font-bold leading-tight mt-2">
-          Enhance efficiency <br /> with our expertise.
-        </h2>
-        <div className="w-[250px] h-[2px] bg-gray-300 mt-6" />
+        <h2 className="text-[20px] md:text-[63px] font-bold leading-tight mt-2">
+  Enhance efficiency{" "}
+  <span className="hidden md:inline"><br /></span>
+  with our expertise.
+</h2>
+
+        <div className="w-[160px] md:w-[250px] h-[2px] bg-gray-300 mt-4" />
       </div>
 
       {/* Timeline container */}
-      <div className="relative mx-auto -mt-20 z-10 w-full">
+      <div className="relative mx-auto -mt-20 z-10 w-full hidden lg:block">
         {/* Background Line Image */}
         <img
           src="/home/Group 1982249958.png"
@@ -151,6 +154,143 @@ const WhyDeventia = () => {
           <a
             href="#"
             className="text-[#FFFFFF] text-md font-semibold hover:underline"
+            >
+            Learn More +
+          </a>
+          </div>
+        </div>
+      </div>
+
+
+      {/*mobile view*/}
+      <div className="relative mx-auto h-[600px] z-10 w-full block lg:hidden ">
+        {/* Background Line Image */}
+        <img
+  src="/home/Group 1982250006.png"
+  alt="Timeline Line"
+  className="w-full h-full object-contain "
+/>
+
+ {/*step 1 */}
+
+
+
+<div className="absolute left-[0%] top-[5%] z-10 sm:max-w-[130px] md:max-w-[250px] flex flex-row gap-3 items-center">
+  <h1 className="text-[#B1B1FE] text-[37px] md:text-[90px] font-semibold leading-none">
+    1
+  </h1>
+
+  {/* Step Content */}
+  <div>
+    <h3 className="text-[10px] md:text-[14px] font-bold mb-1">Planning</h3>
+    <p className="text-[8px] md:text-[12px] text-[#C8CFD5] mb-1">
+      Digital agency is an high test for business website work
+    </p>
+    <a
+      href="#"
+      className="text-[#FFFFFF] text-[8px] md:text-[12px] font-semibold hover:underline"
+    >
+      Learn More +
+    </a>
+  </div>
+</div>
+<div className="absolute right-[0%] top-[15%] z-10 max-w-[130px] md:max-w-[250px] flex flex-row gap-3 items-centr">
+  {/* Step Number */}
+  <h1 className="text-[#B1B1FE] text-[37px] md:text-[90px] font-semibold leading-none">
+    2
+  </h1>
+
+  {/* Step Content */}
+  <div>
+    <h3 className="text-[10px] md:text-[14px] font-bold mb-1">Design </h3>
+    <p className="text-[8px] md:text-[12px] text-[#C8CFD5] mb-1">
+    Digital agency is an high test for business website work    </p>
+    <a
+      href="#"
+      className="text-[#FFFFFF] text-[8px] md:text-[12px] font-semibold hover:underline"
+    >
+      Learn More +
+    </a>
+  </div>
+</div>
+
+
+<div className="absolute right-[0%] top-[50%] z-10 max-w-[130px] md:max-w-[250px] flex flex-row gap-3 items-center">
+  {/* Step Number */}
+  <h1 className="text-[#B1B1FE] text-[37px] md:text-[90px] font-semibold leading-none">
+    4
+  </h1>
+
+  {/* Step Content */}
+  <div>
+    <h3 className="text-[10px] md:text-[14px] font-bold mb-1">Testing</h3>
+    <p className="text-[8px] md:text-[12px] text-[#C8CFD5] mb-1">
+    Digital agency is an high test for business website work
+        </p>
+    <a
+      href="#"
+      className="text-[#FFFFFF] text-[8px] md:text-[12px] font-semibold hover:underline"    >
+      Learn More +
+    </a>
+  </div>
+</div>
+
+
+
+<div className="absolute left-[0%] top-[33%] z-10 max-w-[130px] md:max-w-[250px]  flex flex-row gap-3 items-center">
+          {/* Step Number */}
+          <h1 className="text-[#B1B1FE] text-[37px] md:text-[90px] font-semibold leading-none">
+          3
+  </h1>
+  <div>
+
+          <h3 className="text-[10px] md:text-[14px] font-bold mb-1">Development</h3>
+          <p className="text-[8px] md:text-[12px] text-gray-300 mb-1">
+            Digital agency is an high test for business website work
+          </p>
+          <a
+            href="#"
+            className="text-[#FFFFFF] text-[8px] md:text-[12px] font-semibold hover:underline"
+            >
+            Learn More +
+          </a>
+          </div>
+        </div>
+       
+<div className="absolute left-[0%] bottom-[15%] z-10 max-w-[130px]  md:max-w-[250px] flex flex-row gap-3 items-center">
+          {/* Step Number */}
+          <h1 className="text-[#B1B1FE] text-[37px] md:text-[90px] font-semibold leading-none">
+          5
+  </h1>
+  <div>
+
+          <h3 className="text-[10px] md:text-[14px] font-bold mb-1">Release</h3>
+          <p className="text-[8px] md:text-[10px] text-gray-300 mb-1">
+            Digital agency is an high test for business website work
+          </p>
+          <a
+            href="#"
+            className="text-[#FFFFFF] text-[8px] md:text-[10px] font-semibold hover:underline"
+            >
+            Learn More +
+          </a>
+          </div>
+        </div>
+       
+
+<div className="absolute right-[0%] bottom-[0%] z-10 max-w-[130px] md:max-w-[250px] flex flex-row gap-3 items-center">
+<h1 className="text-[#B1B1FE] text-[37px] md:text-[90px] font-semibold leading-none">
+6
+  </h1>
+  <div>
+
+          <h3 className="text-[10px] md:text-[14px] font-bold mb-1">Support & Improvement</h3>
+          <p className="text-[8px] md:text-[10px] text-gray-300 mb-1">
+            Digital agency is an high test for business website work
+          </p>
+          <a
+            href="#"
+            className="text-[#FFFFFF] text-[8px] md:text-[10px] font-semibold hover:underline"
             >
             Learn More +
           </a>
